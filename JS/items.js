478842600baa146f6fb4d53f6436b8c9f5c7d9a8
Isabelle <isabelle.martinez.wd@gmail.com>
2021-04-20 13:55:14 +0200

    Javascript Items

---
+++ JS/items.js
@@ -0,0 +1,34 @@
+main()
+
+async function main() {
+   const articles = await getArticles()
+
+   for (article of articles) {
+       displayArticle(article)
+   }   
+}
+
+function getArticles() {
+    return fetch("http://localhost:3000/api/teddies")
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(articles) {
+            return articles
+        })
+        .catch(function(error) {
+            alert(error)
+        })
+}
+
+function displayArticle(article) {
+    const templateElt = document.getElementById("templateArticle")
+    const cloneElt = document.importNode(templateElt.content, true)
+
+    cloneElt.getElementById("imageUrl").textContent = article.imageUrl
+    cloneElt.getElementById("name").textContent = article.name
+    cloneElt.getElementById("description").textContent = article.description
+    cloneElt.getElementById("price").textContent = article.price
+
+    document.getElementById("main").appendChild(cloneElt)
+}
