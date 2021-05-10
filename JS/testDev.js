991c2e9cf649ef5370c4ed1e8d44272f24c374e8
Isabelle <isabelle.martinez.wd@gmail.com>
2021-05-10 15:37:15 +0200

    Javascript Shopping cart

--- JS/testDev.js
+++ JS/testDev.js
@@ -21,18 +21,18 @@ else{
         basket = basket + `
         <section class="container-cart content-section">   
             <div id="cartItems" class="cart-items">
-                <div class="cart-row">
+                <div class="cart-row cart-row-items">
                     <div class="cart-item cart-column">
                         <img id="imageUrl" class="cart-item-image" src="${cart[i].image}" width="100"
                             height="100">
                         <div>
                             <span id="name" class="cart-item-title">${cart[i].name}</span><br>
                             <span id="colorOptions" class="cart-item-color mx-2">${cart[i].color}</span>
-                        </div>
+                        </div>    
                     </div>
-                    <span class="cart-price cart-column">${cart[i].price/100}€ </span>
+                    <span id="price" class="cart-price cart-column">${cart[i].price/100}€ </span>
                     <div class="cart-quantity cart-column">
-                        <input class="cart-quantity-input" min="1" type="number" value="${cart[i].quantity}">
+                        <input id="quantity" class="cart-quantity-input" min="1" type="number" value="${cart[i].quantity}">
                         <button class="btn btn-danger" type="button" data-id="${i}"><i class="fa fa-trash-o"></i></button>
                     </div>
                 </div>
@@ -42,12 +42,13 @@ else{
     }
         if(i == cart.length) {
             cartElement.innerHTML = basket; 
-    }}
+        }
+}
 
 // supprimer un article du panier    
-const removeCartItemButtons = document.getElementsByClassName("btn-danger")
-for (let i = 0; i < removeCartItemButtons.length; i++) {
-    const button = removeCartItemButtons[i]
+const removeButtons = document.getElementsByClassName("btn-danger")
+for (let i = 0; i < removeButtons.length; i++) {
+    const button = removeButtons[i]
     button.addEventListener('click', function(event){
         const buttonClicked = event.target
         cart.splice(button.dataset.id,1) // enlève produit du localStorage
@@ -55,24 +56,20 @@ for (let i = 0; i < removeCartItemButtons.length; i++) {
         updateCartTotal()
         localStorage.setItem("shoppingCart", JSON.stringify(cart))
         window.location.reload()
-    })}
+})}
 
-    
-////// RIEN NE MARCHE
-// mise à jour du Total
 function updateCartTotal(){
-    const cartItemContainer = document.getElementsByClassName("cart-items")[0]
-    const cartRows = cartItemContainer.getElementsByClassName("cart-row")
+    const cartRows = document.getElementsByClassName("cart-row-items")
     let total = 0
     for (let i = 0; i < cartRows.length; i++) {
         const cartRow = cartRows[i]
-        const priceElement = CartRow.getElementsByClassName("cart-price")[0]
-        const quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
+        let priceElement = cartRow.getElementsByClassName("cart-price")[0]
+        let quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
+        console.log(priceElement, quantityElement)
         const price = parseFloat(priceElement.innerText.replace("€",""))
-        const quantity = quantityElement.nodeValue
+        const quantity = quantityElement.Value
         total = total + (price * quantity)
     }
-    document.getElementsByClassName("cart-total-price")[0].innerText = total + `€`
+    total = Math.round(total * 100) / 100 // Evite les décimals à l'infini
+    document.getElementsByClassName("cart-total-price")[0].innerText = total + "€"
 }
-
-
