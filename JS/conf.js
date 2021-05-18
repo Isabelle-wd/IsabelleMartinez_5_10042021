420c7538c3f11934c6eaff758e38ce52631040c9
Isabelle-wd <76958473+Isabelle-wd@users.noreply.github.com>
2021-05-18 13:35:53 +0200

    js

--- JS/conf.js
+++ JS/conf.js
@@ -3,12 +3,12 @@ console.log(`responseId : ${responseId}`);
 
 const confElement = document.querySelector("#confElement");
 const confStructure = `
-<h1 class="display-3">Merci pour votre achat!</h1>
-<p class="lead mt-3">Un email de confirmation vous a été envoyé!</p>
-<p>Le numéro de votre commande est : ${responseId}</p>
-<hr>
-<p>Vous avez des questions? <a id="contact" href="mailto:contact@orinoco.fr">Contactez-nous</a></p>       
-<p class="lead"><a class="btn btn-info btn-md mt-5" href="/html/index.html" role="button">Retour à la page d'accueil</a></p>`;
+    <h1 class="display-3">Merci pour votre achat!</h1>
+    <p class="lead mt-3">Un email de confirmation vous a été envoyé!</p>
+    <p>Le numéro de votre commande est : ${responseId}</p>
+    <hr>
+    <p>Vous avez des questions? <a id="contact" href="mailto:contact@orinoco.fr">Contactez-nous</a></p>       
+    <p class="lead"><a class="btn btn-info btn-md mt-5" href="/html/index.html" role="button">Retour à la page d'accueil</a></p>`;
 
 confElement.innerHTML = confStructure;
 
