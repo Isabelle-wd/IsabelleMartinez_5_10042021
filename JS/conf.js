bf547526a35e335f5c627b5c4f6ba3a21f8cff88
Isabelle-wd <76958473+Isabelle-wd@users.noreply.github.com>
2021-05-18 13:09:23 +0200

    Conf page Javascript

--- JS/conf.js
+++ JS/conf.js
@@ -1,3 +1,21 @@
 const responseId = localStorage.getItem("responseId");
+console.log(`responseId : ${responseId}`);
 
-responseId.textContent = ${}
+const confElement = document.querySelector("#confElement");
+const confStructure = `
+<h1 class="display-3">Merci pour votre achat!</h1>
+<p class="lead mt-3">Un email de confirmation vous a été envoyé!</p>
+<p>Le numéro de votre commande est : ${responseId}</p>
+<hr>
+<p>Vous avez des questions? <a id="contact" href="mailto:contact@orinoco.fr">Contactez-nous</a></p>       
+<p class="lead"><a class="btn btn-info btn-md mt-5" href="/html/index.html" role="button">Retour à la page d'accueil</a></p>`;
+
+confElement.innerHTML = confStructure;
+
+function emptyConfPage(key) {
+    localStorage.removeItem(key);
+};
+emptyConfPage("responseId");
+emptyConfPage("shoppingCart");
+
+
